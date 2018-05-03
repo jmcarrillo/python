336d01902f3d858a15f3d22db734bd75ca277293
Manuel Carrillo <inetshell@gmail.com>
2018-05-03 12:18:38 -0500

    Se actualiza URL

--- import.py
+++ import.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 import urllib.request
 
-page = urllib.request.urlopen("http://yahoo.com")
+page = urllib.request.urlopen("http://www.beans-r-us.appspot.com/prices-loyalty.html")
 text = page.read().decode("utf8")
 
 print(text)
