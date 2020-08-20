f40bd737ef2d76d08f8a7dbe5ce537bb58c5e847
manolog <emmanuel.ganster@gmail.com>
2020-08-20 13:31:40 -0500

    copiamos y cambio cuatro lineas

--- import.py
+++ import.py
@@ -10,3 +10,9 @@ elif x > y:
     print('x es mayor que y')
 else:
     print('x e y son iguales')
+if x < y:
+    print("x es menor que y")
+elif x > y:
+    print('x es mayor que y')
+else:
+    print('x e y son iguales')
