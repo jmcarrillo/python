0d195b51623e5d556ce5aa331f993b7fa23040d5
manolog <emmanuel.ganster@gmail.com>
2020-08-21 12:50:17 -0500

    comparacion de x y y con bienvenida

--- import.py
+++ import.py
@@ -2,21 +2,20 @@
 print("Bienvenido a tu practica con python")
 
 
-x = 15
-y = 15
-if x < y:
-    parint("x es menor que y")
-elif x > y:
-    print('x es mayor que y')
-else:
-    print('x e y son iguales')
+x = 4
+y = 24
 if x < y:
     print("x es menor que y")
 elif x > y:
     print('x es mayor que y')
 else:
-    print('x e y son iguales'
-            )
+    print('x e y son iguales')
+if x <y:
+    print("x es menor que y")
+elif x > y:
+    print('x es mayor que y')
+else:
+    print('x e y son iguales' )
 if x < y:
     print("x es menor que y")
 elif x > y:
